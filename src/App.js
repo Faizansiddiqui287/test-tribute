9e678c4901059ca3ad59320064b89cb3cc72f002
Faizan Siddiqui <91340388+Faizansiddiqui287@users.noreply.github.com>
2022-02-13 19:53:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+import img1 from './images/lata-mangeshkar-bharat-ratna.jpg';
+
+function App() {
+  return (
+    <div className="App">
+
+      <nav className='nav'><h3>Lata Mangehskar- The Pride of India</h3>
+        <div><h3>28 September 1929 – 6 February 2022</h3></div>
+      </nav>
+      <img src={img1} className='pic' />
+      
+      <div><h3>Lata Mangehskar (28/09/1929 – 6/02/2022)</h3></div>
+      <h1>WHO IS LATA MANGESHKAR
+      </h1>
+      <p>Lata Mangeshkar, also known as the 'Nightingale of India', was one of the most versatile singers in the Indian film industry. Lata was born on September 28, 1929, to classical singer and theatre artist Pandit Deenanath Mangeshkar and Shevanti in Indore, Madhya Pradesh. Her father started teaching her music at an early age. By the time she was five, Lata was seen participating as an actress in plays written by her father. Her siblings  - Meena, Asha, Usha, and Hridaynath - are all accomplished singers and musicians.
+
+        In a career spanning nearly eight decades, Lata Mangeshkar was the singing voice for a bevy of Bollywood leading ladies. She lent her voice to over 5,000 songs in over a thousand Hindi and 36 regional films. She had an unprecedented impact on Indian film music. Since 1942, Lata pushed back the boundaries of music with her mind-boggling skills. Over the years, Lata had sung for actresses from Madhubala to Priyanka Chopra. Best known for her versatile voice quality, she recorded albums of all kinds (ghazals, pop, etc). </p>
+      <h1>Take a look at Lata Mangeshkar's musical journey</h1>
+      <p>In 1942, her father's untimely death put the onus of providing for the family on her shoulders. Pandit Deenanath's friend Master Vinayak took care of the Mangeshkar family and offered her a role in the film Badi Maa. In 1949, she moved to Bombay (now Mumbai), where she began learning Hindustani music from Ustad Aman Ali Khan.
+
+        Lata had worked with several legendary music directors including Madan Mohan, R D Burman, the duo Laxmikant-Pyarelal and A R Rahman. She teamed up with Madan Mohan for the 1960s like Aap Ki Nazron Ne Samjha from Anpadh, Lag Jaa Gale and Naina Barse Rim Jhim from Woh Kaun Thi?. Lata had sung over 700 songs for Laxmikant-Pyarelal, which include Mere Naseeb Mein from Naseeb and Sheesha Ho Ya Dil Ho from Asha.</p>
+
+    </div>
+  );
+}
+
+export default App;
